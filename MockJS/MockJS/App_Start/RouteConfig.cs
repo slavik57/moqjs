1524cb440a6556f5b286aa11775e7fb62c22ad7b
slavik57 <slavik57@gmail.com>
2015-03-14 17:36:50 +0200

    Add qunit

--- MockJS/MockJS/App_Start/RouteConfig.cs
+++ MockJS/MockJS/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace MockJS
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Tests", action = "Index" }
             );
         }
     }
