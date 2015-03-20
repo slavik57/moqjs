ad1c54409d1b8a249b33c6f9830372ba795cece3
slavik57 <slavik57@gmail.com>
2015-03-20 16:55:22 +0200

    - Add ItIsAny and ItIsAny tests

---
+++ MockJS/MockJS/Scripts/It.ts
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+module mockJS {
+    export interface IItIs {
+        match(argument: any): boolean;
+    }
+
+    class ItIsAny implements IItIs {
+        constructor(public expectedType: Function) {
+        }
+
+        match(argument: any): boolean {
+            if (argument === null ||
+                argument === undefined) {
+                return false;
+            }
+
+            return argument.constructor === this.expectedType ||
+                argument instanceof this.expectedType;
+        }
+    }
+
+    export class It {
+        public static isAny(type: Function): IItIs {
+            return new ItIsAny(type);
+        }
+    }
+}
